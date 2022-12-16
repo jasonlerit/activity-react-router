b932a3136f5598e2650368fd647d14398f597e9f
jomar567 <cladojomar881@gmail.com>
2022-12-16 09:18:41 +0800

    fix contact info

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -2,7 +2,12 @@
 const ContactInfo = () => {
     return (
         <div className="border-2 border-dark border h-100 d-flex justify-content-center align-items-center">
-            <h3>Contact Info</h3>
+            <div>
+                <h3 className="mb-3">Contact Us:</h3>
+                <h5>+8127-821-5130</h5>
+                <h5>Nihommatsu-shi, Fukushima <br />
+                Japan</h5>
+            </div>
         </div>
     )
 }
--- src/components/contact/GoogleMap.jsx
+++ src/components/contact/GoogleMap.jsx
@@ -5,12 +5,12 @@ const GoogleMap = () => {
     return (
         <div className='border-2 border-dark border p-3'>
             <div>
-                <h3>Group Address Here</h3>
+                <h3>Group Address:</h3>
             </div>
             <div className="mapouter">
                 <div className="gmap_canvas">
-                    <iframe className="gmap_iframe" width="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=1564&amp;height=289&amp;hl=en&amp;q=Tugawe Cove Resort&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-                    <a href="https://piratebay-proxys.com/%22%3EPiratebay%3C/a%3E" />
+                    <iframe className="gmap_iframe" width="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?q=Nihommatsu-shi,%20Fukushima%20Japan&t=k&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+                    <a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/" />
                 </div>
             </div>
         </div>
