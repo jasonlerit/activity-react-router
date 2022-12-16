18fc2ce5af2f8688090d93f8c6d8c75d9b2207dc
jasonlerit <jasonlerit077@gmail.com>
2022-12-16 08:38:40 +0800

    updated attribute

--- src/components/contact/GoogleMap.jsx
+++ src/components/contact/GoogleMap.jsx
@@ -7,10 +7,10 @@ const GoogleMap = () => {
             <div>
                 <h3>Group Address Here</h3>
             </div>
-            <div class="mapouter">
-                <div class="gmap_canvas">
-                <iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=1564&amp;height=289&amp;hl=en&amp;q=Tugawe Cove Resort&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
-                <a href="https://piratebay-proxys.com/%22%3EPiratebay%3C/a%3E" />
+            <div className="mapouter">
+                <div className="gmap_canvas">
+                    <iframe className="gmap_iframe" width="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=1564&amp;height=289&amp;hl=en&amp;q=Tugawe Cove Resort&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                    <a href="https://piratebay-proxys.com/%22%3EPiratebay%3C/a%3E" />
                 </div>
             </div>
         </div>
